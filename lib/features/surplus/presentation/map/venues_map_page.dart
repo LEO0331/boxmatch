9660ab4300ad0be50f369e0ba6cec932bad3830c
leo <25032781+LEO0331@users.noreply.github.com>
2026-04-01 15:12:15 +0800

    fix mapping issue

--- lib/features/surplus/presentation/map/venues_map_page.dart
+++ lib/features/surplus/presentation/map/venues_map_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 import 'package:latlong2/latlong.dart';
 
 import '../../../../app/app_scope.dart';
@@ -46,9 +47,12 @@ class VenuesMapPage extends StatelessWidget {
                       ),
                       children: [
                         TileLayer(
+                          // CARTO tiles are generally CORS-friendly for web demos.
                           urlTemplate:
-                              'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                              'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png',
+                          subdomains: const ['a', 'b', 'c', 'd'],
                           userAgentPackageName: 'com.example.boxmatch',
+                          tileProvider: CancellableNetworkTileProvider(),
                         ),
                         MarkerLayer(
                           markers: venues
